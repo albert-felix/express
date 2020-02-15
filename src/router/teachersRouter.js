12bf958e13b559f26ee50260dd838aaa4b8ad7fc
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2020-02-15 20:57:28 +0530

    TeachersRouter updated

--- src/router/teachersRouter.js
+++ src/router/teachersRouter.js
@@ -4,8 +4,34 @@ const teachersDB = require("../teaachersDb/teachersDb");
 const teachersRouter = express.Router();
 
 teachersRouter
-  .get("/",(req,res) => {
-    res.status(200).json({teachers : teachersDB})
+  .get("/", (req, res) => {
+    res.status(200).json({ teachers: teachersDB });
+  })
+
+  .post("/", (req, res) => {
+    req.body.forEach(teacher => {
+      if (teacher.id && teacher.firstName) {
+        teachersDB.push(teacher);
+        res.status(200).json({ Message: "Teachers data updated successfully" });
+      } else {
+        res.status(400).send("Data invalid");
+      }
+    });
+  })
+
+  .delete("/", (req, res) => {
+    req.body.forEach(id => {
+      let requiredTeacherIndex;
+      teachersDB.forEach((teacher, teacherIndex) => {
+        if (id === teacher.id) {
+          requiredTeacherIndex = teacherIndex;
+          teachersDB.splice(requiredTeacherIndex,1);
+          res.status(200).json({Message : "Teachers data deleted"});
+        }else {
+          res.status(404).send("Data Not Found");
+        }
+      });
+    });
   });
 
-  module.exports = teachersRouter;
+module.exports = teachersRouter;
