89b31d59d24d28d3187a1a8978f0f016f36d7112
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2020-02-16 08:38:25 +0530

    Completed with documentation

--- src/router/teachersRouter.js
+++ src/router/teachersRouter.js
@@ -10,28 +10,27 @@ teachersRouter
 
   .post("/", (req, res) => {
     req.body.forEach(teacher => {
+      console.log(teacher)
       if (teacher.id && teacher.firstName) {
         teachersDB.push(teacher);
         res.status(200).json({ Message: "Teachers data updated successfully" });
       } else {
         res.status(400).send("Data invalid");
       }
-    });
+    })
   })
 
   .delete("/", (req, res) => {
-    req.body.forEach(id => {
-      let requiredTeacherIndex;
+    req.body.id.forEach(id => {
       teachersDB.forEach((teacher, teacherIndex) => {
+        let requiredTeacherIndex;
         if (id === teacher.id) {
           requiredTeacherIndex = teacherIndex;
           teachersDB.splice(requiredTeacherIndex,1);
           res.status(200).json({Message : "Teachers data deleted"});
-        }else {
-          res.status(404).send("Data Not Found");
         }
-      });
-    });
+    })
+    })
   });
 
 module.exports = teachersRouter;
